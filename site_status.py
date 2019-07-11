d721ba48fc832400114be8549bb7c4c5fb4e2504
Robert Freeman <robertefreeman@users.noreply.github.com>
2019-07-11 14:15:42 -0500

    building out framework for site_status.py

--- site_status.py
+++ site_status.py
@@ -1,8 +1,5 @@
 import requests
-
-
-r = requests.get('https://www.google.com')
-print(f'The current status code is {r.status_code}')
+import sys
 
 def url_normalizer(url):
     pass
@@ -10,3 +7,16 @@ def url_normalizer(url):
 def timer():
     pass
     
+def check_site(url):
+	for addy in url:
+		url_normalizer(addy)
+		r = requests.get(addy)
+		print(f'The current status code for {addy} is:  {r.status_code}')
+		
+
+check_site(sys.argv)
+
+	
+
+	
+
