3e1277546b1e3ec076faf43f2350bd120261b6cb
Robert Freeman <robert@Roberts-MacBook-Pro.local>
2019-07-11 13:23:19 -0500

    added site_status fiddle

---
+++ site_status.py
@@ -0,0 +1,12 @@
+import requests
+
+
+r = requests.get('https://www.google.com')
+print(f'The current status code is {r.status_code}')
+
+def url_normalizer(url):
+    pass
+
+def timer():
+    pass
+    
