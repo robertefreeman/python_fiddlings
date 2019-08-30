811f88a95f5bc3951ee8f66c9f8abf8cb7965dd4
Robert Freeman <robert@github.com>
2019-08-30 11:06:56 -0400

    created azure sentiment and language indentification snippet

---
+++ azure_ai_test.py
@@ -0,0 +1,51 @@
+import requests  # requests is used to make the api call
+from pprint import pprint  # pprint is used to format the json response
+import os # os used to grab env variables
+
+# Pull env variables from os for Azure cognitive services keys
+
+key_var_name = 'TEXT_ANALYTICS_SUBSCRIPTION_KEY'
+if not key_var_name in os.environ:
+    raise Exception('Please set/export the environment variable: {}'.format(key_var_name))
+subscription_key = os.environ[key_var_name]
+
+endpoint_var_name = 'TEXT_ANALYTICS_ENDPOINT'
+if not endpoint_var_name in os.environ:
+    raise Exception('Please set/export the environment variable: {}'.format(endpoint_var_name))
+endpoint = os.environ[endpoint_var_name]
+
+# Append languages to text analytics end point
+
+language_api_url = endpoint + "/languages"
+
+documents = {"documents": [
+    {"id": "1", "text": "This is a document written in English."},
+    {"id": "2", "text": "Este es un document escrito en Español."},
+    {"id": "3", "text": "这是一个用中文写的文件"}
+]}
+
+headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+response = requests.post(language_api_url, headers=headers, json=documents)
+languages = response.json()
+pprint(languages)
+
+print('\n\n\n\n')
+
+
+sentiment_url = endpoint + "/sentiment"
+
+documents = {"documents": [
+    {"id": "1", "language": "en",
+        "text": "Now that you have an idea of how a simple Flask app works."},
+    {"id": "2", "language": "en",
+        "text": "I had a terrible time at the hotel. The staff was rude and the food was awful."},
+    {"id": "3", "language": "es",
+        "text": "Los caminos que llevan hasta Monte Rainier son espectaculares y hermosos."},
+    {"id": "4", "language": "es",
+     "text": "La carretera estaba atascada. Había mucho tráfico el día de ayer."}
+]}
+
+headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+response = requests.post(sentiment_url, headers=headers, json=documents)
+sentiments = response.json()
+pprint(sentiments)
